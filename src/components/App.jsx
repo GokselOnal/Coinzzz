087abf212bf2ac843d92133cd21b1e734a99cdc9
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2021-05-24 00:25:58 +0300

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Sign_in from "./Sign_in";
-import coins from "../coins";
 
 function App(){
   return(
--- src/components/App3.jsx
+++ src/components/App3.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Register from "./Register";
 
 function App(){
   return(
     <div>
       <Header />
-      <Register />
+
       <Footer />
     </div>
   );
 }
 
-export default App2;
+export default App3;
