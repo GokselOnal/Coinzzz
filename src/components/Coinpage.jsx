074da60f3d15b4d63863065f34279890a60b2ebb
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2021-05-26 02:48:15 +0300

    Generic coinpages information are updated

--- src/components/Coinpage.jsx
+++ src/components/Coinpage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import coins from "../coins"
 
 function Coinpage(props){
   return(
     <div>
       <Header />
+      <h2>{props.name}</h2>
+      <h2>{props.buy_price}</h2>
       <p>{props.description}</p>
       <Footer />
     </div>
--- src/index.js
+++ src/index.js
@@ -9,16 +9,24 @@ import coins from "./coins"
 ReactDOM.render(
   <div>
     <Coinpage
+    name={coins[0].name}
+    buy_price={coins[0].buy_price}
     description={coins[0].description}
     />
     <Coinpage
+    name={coins[1].name}
+    buy_price={coins[1].buy_price}
     description={coins[1].description}
     />
     <Coinpage
-    description={coins[1].description}
+    name={coins[2].name}
+    buy_price={coins[2].buy_price}
+    description={coins[2].description}
     />
     <Coinpage
-    description={coins[1].description}
+    name={coins[3].name}
+    buy_price={coins[3].buy_price}
+    description={coins[3].description}
     />
   </div>,
   document.getElementById('login')
